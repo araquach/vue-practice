68c316dd91f8262338f3aea9d36e907e20fc6035
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-11-28 15:10:34 +0000

    added new validation practice module

---
+++ src/components/Validation/Validator.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1 class="title is-4">Validate</h1>
+    </div>
+</template>
+<script>
+
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import User from "./components/router/user/User"
 import Home from "./components/router/Home"
+import Validation from './components/validation/Validator'
 
 export const routes = [
     { path: '', component: Home},
-    { path: '/user/:id', component: User}
+    { path: '/user/:id', component: User},
+    { path: '/validation', component: Validation}
 ]
