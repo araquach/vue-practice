0552b93e5ba5fb418c8221b03a899f0eb4f68c76
Adam Carter <adam@contra-design.com>
2020-01-29 16:18:50 +0000

    Got the form workin - I thnk

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import Buefy from 'buefy'
 import { routes } from "./routes"
 
+
 Vue.use(Buefy)
+Vue.use(Vuelidate)
 Vue.use(VueRouter)
 
 import { store } from './store/store'
+
 
 window.axios = require('axios')
 
--- src/components/HolidayCreate.vue
+++ src/components/HolidayCreate.vue
@@ -6,21 +6,33 @@
             <hr>
             <h1 class="title is-4">Book a Holiday</h1>
             <form @submit.prevent="submit">
-                <b-field label="Staff">
-                    <b-input v-model="staff"
+                <b-field label="Staff"
+                         :type="{ 'is-danger': $v.staff.$error }"
+                         :message="{'Staff Required' : !staff.required}">
+                    <b-input v-model.trim="$v.staff.$model"
                              placeholder="Staff">
                     </b-input>
                 </b-field>
-                <b-field label="Days">
-                    <b-numberinput v-model="days"
+                <b-field label="Days"
+                         :type="{ 'is-danger': $v.days.$error }"
+                         :message="{'Days required' : !days.required}">
+                    <b-numberinput v-model.trim="$v.days.$model"
                                    placeholder="Days">
                     </b-numberinput>
                 </b-field>
                 <br>
                 <div class="field">
                     <div class="control">
-                        <button class="button is-primary" type="submit">Book Holiday</button>
+                        <button class="button is-primary"
+                                type="submit"
+                                :disabled="submitStatus === 'PENDING'">
+                            Book Holiday
+                        </button>
                     </div>
+                </div>
+
+                <div v-if="submitStatus === 'OK'">
+                    <p class="is-size-4 has-text-primary">Holiday request submitted</p>
                 </div>
             </form>
         </div>
@@ -28,27 +40,44 @@
 </template>
 
 <script>
+    import {required, numeric, minValue} from 'vuelidate/lib/validators'
+
     export default {
         data() {
             return {
-                staff : null,
-                days: null
+                staff : '',
+                days: 0,
+                submitStatus: null
             }
         },
 
-        methods:{
+        validations: {
+            staff: {required},
+            days: {
+                required,
+                numeric,
+                minValue: minValue(1)
+            }
+        },
+
+        methods: {
             submit() {
                 console.log('submit!')
-                axios.post('/api/holiday', {
-                    staff: this.staff,
-                    days: this.days
-                })
-                    .then(response => {
-                        this.submitStatus = 'OK'
+                this.$v.$touch()
+                if (this.$v.$invalid) {
+                    this.submitStatus = 'ERROR'
+                } else {
+                    axios.post('/api/holiday', {
+                        staff: this.staff,
+                        days: this.days
                     })
-                    .catch((e) => {
-                        console.error(e)
-                    })
+                        .then(response => {
+                            this.submitStatus = 'OK'
+                        })
+                        .catch((e) => {
+                            console.error(e)
+                        })
+                }
             }
         }
     }
