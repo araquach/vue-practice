b7759a50c6f958d99ffde97bd38a96929b91d6d8
Adam Carter <adam@contra-design.com>
2020-02-04 16:46:35 +0000

    Got the sat function workling

--- src/components/HolidayCreate.vue
+++ src/components/HolidayCreate.vue
@@ -21,10 +21,12 @@
                             icon="calendar-today">
                     </b-datepicker>
                 </b-field>
-                <div class="field">
-                    <label>Saturdays</label>
-                    <input v-model="saturdays">
-                </div>
+                <b-field label="Saturdays">
+                    <b-input v-model="saturdays"></b-input>
+                    <button class="button" @click="saturdays -= .5">-</button>
+                    <button class="button" @click="saturdays += .5">+</button>
+                </b-field>
+                <p>Total Saturdays: {{totalSat}}</p>
                 <div class="field">
                     <button class="button" type="submit">Submit</button>
                 </div>
@@ -35,13 +37,39 @@
 
 <script>
     import {mapMutations} from 'vuex'
-
+    import moment from 'moment'
     export default {
         data() {
             return {
                 start_date: null,
                 end_date: null,
                 saturdays: 0
+            }
+        },
+
+        computed: {
+            totalSat() {
+                const start = moment(this.start_date)
+                const end = moment(this.end_date)
+
+                const dailyInfo = [false, false, false, false, false, false, true]
+                let totalDays = 0;
+
+                dailyInfo.forEach((info, index) => {
+                    if (info === true) {
+                        let current = start.clone();
+                        if (current.isoWeekday() <= index) {
+                            current = current.isoWeekday(index);
+                        } else {
+                            current.add(1, 'weeks').isoWeekday(index);
+                        }
+                        while (current.isSameOrBefore(end)) {
+                            current.day(7 + index);
+                            totalDays += 1;
+                        }
+                    }
+                })
+                return this.saturdays = totalDays
             }
         },
 
